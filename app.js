041661c4579d8eb2f64e459ea1628e4cadcc36db
Jeimmy25 <jeimmy0799@gmail.com>
2025-03-14 17:32:23 -0500

    Actualiza funcion agregar amigo

--- app.js
+++ app.js
@@ -7,16 +7,19 @@ function agregarAmigo() {
     let nombre = inputAmigo.value.trim();
     
     if (nombre !== '') {
-      // Creamos un elemento li para el nuevo amigo
-      let li = document.createElement('li');
-      li.textContent = nombre;
-      
-      // Agregamos el elemento li a la lista
-      listaAmigos.appendChild(li);
-      
-      // Limpiamos el campo de texto
-      inputAmigo.value = '';
-    } else {
-      alert('Por favor, ingrese un nombre');
+        //// Añadimos el nombre al arreglo de amigos
+        amigos.push(nombre);
+    
+        // Creamos un elemento li para el nuevo amigo
+        let li = document.createElement('li');
+        li.textContent = nombre;
+        
+        // Agregamos el elemento li a la lista
+        listaAmigos.appendChild(li);
+        
+        // Limpiamos el campo de texto
+        inputAmigo.value = '';
+      } else {
+        alert('Por favor, ingrese un nombre');
+      }
     }
-  }
